fef1409718fdd2c612de02101ea0a3d743f21bd6
alamirfarma <alamirfarma@gmail.com>
2026-04-27 10:48:45 +0800

    Update APPS_SCRIPT_URL to new Google Apps Script URL

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Google Apps Script Web App URL
 // URL sudah dikonfigurasi dan siap digunakan
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby09ZOQeakrC0k-YS3E1I47udCJmWtEDE7tY5KazzLw4uqwdTXD7T9xWPny7OIi0uAVbg/exec';
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz3CMaeoQHcwOueHKVX-MW-5x59iJsniC9iWGhjY6_zfPcgZjqTzIyuY1eZVAdAQX2a9Q/exec';
 
 // Spreadsheet ID
 export const SPREADSHEET_ID = '159UdTU4A3CSFKTzy34QDEXItCi2w7zmKXQBPoBOaaO0';
